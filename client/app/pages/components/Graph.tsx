81e6b6f84b234bcf0b24a0c259cef17a8dfa36b3
Noah <na933950@gmail.com>
2024-03-16 18:48:55 -0700

    Changes

---
+++ client/app/pages/components/Graph.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Graph = () => {
+  return (
+    <div className="h-1/4 bg-slate-800 w-full rounded-xl box-border">
+    </div>
+  );
+}
+
+export default Graph
---
+++ client/app/pages/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="h-3/4 bg-slate-800 w-full rounded-xl box-border">
+    </div>
+  );
+};
+
+export default Sidebar;
